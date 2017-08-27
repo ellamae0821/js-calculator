af18fc934b6c1334cdd55ecc11cc3766da3331b9
Ella Mae A <ellamae207@gmail.com>
2017-08-27 08:21:54 -1000

    passes 1 -  #multiply

--- calculator.js
+++ calculator.js
@@ -6,36 +6,63 @@
  * @return {object} `calculator` object that can be used
  */
 
+var calculatorModule = function (){
+  var memory = 0;
+  var total = 0;
+
 
   /**
    * sets the `total` to the number passed in
    * @param  { Number } x
    * @return { Number }    current total
    */
-
+   function load (x){
+    inspect(newTotal);
+    total = x;
+    return total;
+   }
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
 
+   function getTotal (){
+    return total;
+   }
+
 
   /**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
+   function add(num){
+    inspect(num);
+    total += num;
+    return total;
+   }
 
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
+   function subtract(num){
+    inspect(num);
+    total -= num;
+    return total;
+   }
 
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
+   function multiply(num){
+    inspect(num);
+    total *= num;
+    return total;
+   }
 
 
   /**
@@ -63,3 +90,11 @@
    * Validation
    */
 
+  function inspect(num1){
+    if(typeof num1 !== "number"){
+      throw new Error("Hey, I'mma give you Lickins!");
+    }
+  }
+
+
+};
